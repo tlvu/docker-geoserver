c0e17dc06ed02f337d9d3850c5e143141464438f
Long Vu <vu.long@ouranos.ca>
2023-03-30 22:30:28 -0400

    scripts/entrypoint.sh: avoid chown GEOWEBCACHE_CACHE_DIR since this dir is large and slow down Geoserver startup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -96,7 +96,7 @@ export JAVA_OPTS="${JAVA_OPTS} ${GEOSERVER_OPTS}"
 chown -R "${USER_NAME}":"${GEO_GROUP_NAME}" "${CATALINA_HOME}" "${FOOTPRINTS_DATA_DIR}" \
 "${CERT_DIR}" "${FONTS_DIR}"  /home/"${USER_NAME}"/ "${COMMUNITY_PLUGINS_DIR}" "${STABLE_PLUGINS_DIR}" \
 "${GEOSERVER_HOME}" "${EXTRA_CONFIG_DIR}"  /usr/share/fonts/ /scripts /tomcat_apps.zip \
-/tmp/ "${GEOWEBCACHE_CACHE_DIR}";chmod o+rw "${CERT_DIR}";chmod 400 ${CATALINA_HOME}/conf/*
+/tmp/ ;chmod o+rw "${CERT_DIR}";chmod 400 ${CATALINA_HOME}/conf/*
 
 if [[ -f ${GEOSERVER_HOME}/start.jar ]]; then
   exec gosu ${USER_NAME} ${GEOSERVER_HOME}/bin/startup.sh
